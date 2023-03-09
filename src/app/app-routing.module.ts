e2270ced189600619214511336238f23b4ab7d40
Mahmoud Khamis Ahmed <68229221+mkhamis266@users.noreply.github.com>
2023-03-09 11:24:21 +0200

    create static routes and navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
+import { AboutComponent } from './pages/about/about.component';
+import { TermsAndConditionsComponent } from './pages/terms-and-conditions/terms-and-conditions.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'category', component: SingleCategoryComponent },
+  { path: 'post', component: SinglePostComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'terms-conditions', component: TermsAndConditionsComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
