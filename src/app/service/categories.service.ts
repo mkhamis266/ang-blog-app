8b7b2a22293ae02e99c42d9b3f71fe9d9bf8bc80
Mahmoud Khamis Ahmed <68229221+mkhamis266@users.noreply.github.com>
2023-04-10 10:21:33 +0200

    categories service

---
+++ src/app/service/categories.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { map } from 'rxjs';
+import { Category } from '../models/category';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoriesService {
+  constructor(private afs: AngularFirestore) {}
+
+  getCategories() {
+    this.afs
+      .collection('categories')
+      .snapshotChanges()
+      .pipe(
+        map((actions) => {
+          return actions.map((action) => {
+            const data: Category = action.payload.doc.data() as Category;
+            const id: string = action.payload.doc.id;
+            return { data, id };
+          });
+        })
+      );
+  }
+}
