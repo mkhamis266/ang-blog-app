7183829c8ed0712ddedca9157ab7749d3ae8ef5d
Mahmoud Khamis Ahmed <68229221+mkhamis266@users.noreply.github.com>
2023-04-10 13:40:41 +0200

    post data as input in post card

--- src/app/layouts/post-card/post-card.component.ts
+++ src/app/layouts/post-card/post-card.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Post } from 'src/app/models/post';
 
 @Component({
   selector: 'app-post-card',
   templateUrl: './post-card.component.html',
-  styleUrls: ['./post-card.component.scss']
+  styleUrls: ['./post-card.component.scss'],
 })
 export class PostCardComponent implements OnInit {
-
-  constructor() { }
-
+  constructor() {}
+  @Input() postData!: { id: string; data: Post };
   ngOnInit(): void {
+    console.log(this.postData);
   }
-
 }
