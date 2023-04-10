7714fe05eb158b2d9e0d0f4224a419c5da3b0bf6
Mahmoud Khamis Ahmed <68229221+mkhamis266@users.noreply.github.com>
2023-04-10 13:40:21 +0200

    get featured post as observable

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,22 @@
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/models/post';
+import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  featuredPosts$!: Observable<{ id: string; data: Post }[]>;
+  isFeaturedLoading: boolean = true;
+  constructor(private postsService: PostsService) {
+    this.featuredPosts$ = postsService.getFeaturedPosts();
+    this.postsService.getFeaturedPosts().subscribe((data) => {
+      this.isFeaturedLoading = false;
+    });
   }
 
+  ngOnInit(): void {}
 }
