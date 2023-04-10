3e35e8bbea1dc6c22dcb83962bf5193cea0089e7
Mahmoud Khamis Ahmed <68229221+mkhamis266@users.noreply.github.com>
2023-04-10 14:40:41 +0200

    load categoryPosts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'category', component: SingleCategoryComponent },
+  { path: 'category/:id', component: SingleCategoryComponent },
   { path: 'post', component: SinglePostComponent },
   { path: 'about', component: AboutComponent },
   { path: 'terms-conditions', component: TermsAndConditionsComponent },
--- src/app/pages/single-category/single-category.component.ts
+++ src/app/pages/single-category/single-category.component.ts
@@ -1,15 +1,34 @@
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Post } from 'src/app/models/post';
+import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
   selector: 'app-single-category',
   templateUrl: './single-category.component.html',
-  styleUrls: ['./single-category.component.scss']
+  styleUrls: ['./single-category.component.scss'],
 })
 export class SingleCategoryComponent implements OnInit {
-
-  constructor() { }
+  categoryId!: string;
+  categoryPosts$!: Observable<{ id: string; data: Post }[]>;
+  isLoading = true;
+  constructor(
+    private route: ActivatedRoute,
+    private postsService: PostsService
+  ) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.categoryId = params['id'];
+      this.categoryPosts$ = this.postsService.getPostsByCategory(
+        this.categoryId
+      );
+      this.postsService
+        .getPostsByCategory(this.categoryId)
+        .subscribe((data) => {
+          this.isLoading = false;
+        });
+    });
   }
-
 }
