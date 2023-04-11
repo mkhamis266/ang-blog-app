86cd3b97559f9e13865d131623c4978793541b55
Mahmoud Khamis Ahmed <68229221+mkhamis266@users.noreply.github.com>
2023-04-11 10:04:45 +0200

    get post id form route

--- src/app/pages/single-post/single-post.component.ts
+++ src/app/pages/single-post/single-post.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-single-post',
   templateUrl: './single-post.component.html',
-  styleUrls: ['./single-post.component.scss']
+  styleUrls: ['./single-post.component.scss'],
 })
 export class SinglePostComponent implements OnInit {
-
-  constructor() { }
+  postId!: string;
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.postId = params['id'];
+    });
   }
-
 }
