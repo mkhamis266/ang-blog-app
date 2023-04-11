d8c535c52e2e3be395e27b593fd20b82524c2a0e
Mahmoud Khamis Ahmed <68229221+mkhamis266@users.noreply.github.com>
2023-04-11 11:25:56 +0200

    show similar posts dynamic

--- src/app/pages/single-post/single-post.component.ts
+++ src/app/pages/single-post/single-post.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { Post } from 'src/app/models/post';
 import { PostsService } from 'src/app/services/posts.service';
 
@@ -13,6 +13,8 @@ export class SinglePostComponent implements OnInit {
   postId!: string;
   postData!: Post;
   postDataLoading = true;
+  similarPosts$!: Observable<{ id: string; data: Post }[]>;
+  similarPostsLoading = true;
   constructor(
     private route: ActivatedRoute,
     private postsService: PostsService
@@ -24,6 +26,18 @@ export class SinglePostComponent implements OnInit {
       this.postsService.getPostById(this.postId).subscribe((post) => {
         this.postData = post;
         this.postDataLoading = false;
+        this.similarPosts$ = this.postsService
+          .getPostsByCategory(this.postData.category.categoryId)
+          .pipe(
+            map((postsArray) => {
+              return postsArray.filter(
+                (post) => post.data.title != this.postData.title
+              );
+            })
+          );
+        this.similarPosts$.subscribe((res) => {
+          this.similarPostsLoading = false;
+        });
       });
     });
   }
