aed30c0f55e492e092a0e535b63424fc92c4e77c
Mahmoud Khamis Ahmed <68229221+mkhamis266@users.noreply.github.com>
2023-04-12 11:12:33 +0200

    add toastr message

--- src/app/services/subscription.service.ts
+++ src/app/services/subscription.service.ts
@@ -1,19 +1,21 @@
+import { ToastrModule } from 'ngx-toastr';
 import { Injectable } from '@angular/core';
 import { Sub } from '../models/sub';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SubscriptionService {
-  constructor(private afs: AngularFirestore) {}
+  constructor(private afs: AngularFirestore, private toastr: ToastrService) {}
 
   addSub(subscriptionData: Sub) {
     this.afs
       .collection('subscriptions')
       .add(subscriptionData)
       .then(() => {
-        console.log('data added to subscription');
+        this.toastr.success('Subscription added successfully');
       });
   }
 }
