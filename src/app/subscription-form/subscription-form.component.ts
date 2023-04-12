d2c68fc6987b2b216fd5065e50e5c58eb0f13a3b
Mahmoud Khamis Ahmed <68229221+mkhamis266@users.noreply.github.com>
2023-04-12 10:48:00 +0200

    Subscription form

--- src/app/subscription-form/subscription-form.component.ts
+++ src/app/subscription-form/subscription-form.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Sub } from '../models/sub';
 
 @Component({
   selector: 'app-subscription-form',
   templateUrl: './subscription-form.component.html',
-  styleUrls: ['./subscription-form.component.scss']
+  styleUrls: ['./subscription-form.component.scss'],
 })
 export class SubscriptionFormComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onSubmit(form: any) {
+    const subData: Sub = form.value;
   }
-
 }
