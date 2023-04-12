78bac542c62874f6bbbca9e0269df85b27b65be7
Mahmoud Khamis Ahmed <68229221+mkhamis266@users.noreply.github.com>
2023-04-12 11:13:00 +0200

    send Subscription to db

--- src/app/subscription-form/subscription-form.component.ts
+++ src/app/subscription-form/subscription-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Sub } from '../models/sub';
+import { SubscriptionService } from '../services/subscription.service';
 
 @Component({
   selector: 'app-subscription-form',
@@ -7,10 +8,11 @@ import { Sub } from '../models/sub';
   styleUrls: ['./subscription-form.component.scss'],
 })
 export class SubscriptionFormComponent implements OnInit {
-  constructor() {}
+  constructor(private subService: SubscriptionService) {}
 
   ngOnInit(): void {}
   onSubmit(form: any) {
     const subData: Sub = form.value;
+    this.subService.addSub(subData);
   }
 }
